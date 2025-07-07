13cc792a1f3ad00fb5d226a6d0ae137764ef2d82
l-yeong <yahn10@naver.com>
2025-07-07 11:51:28 +0900

    실습6 10번문제 하기

--- src/종합/예제1/BoardService1.java
+++ src/종합/예제1/BoardService1.java
@@ -4,69 +4,71 @@ import java.util.Scanner;
 
 public class BoardService1 {
     public static void main(String[] args) {
-        //step1: 반복적인 메인(화면)출력
-        //step2: 입력 선택에 따른 화면 출력
-        //step3: 기능별 필요한 메모리 설계
-        //step4: 각 기능별 세부 코드 구현
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        String content1=null; String writer1=null; String content2=null;
-        String writer2=null;  String content3=null; String writer3=null;
+        String phone1 = null;
+        String phone2 = null;
+        String phone3 = null;
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0;
 
-        for( ; ; ){ // 무한루프 시작 // for Start
-            System.out.println("============= My Community =============");
-            System.out.println("        1.게시물쓰기 | 2.게시물출력");
+        int total = 0;
+        for (; ; ) {
+            System.out.println("=========== 맛집 대기 시스템 ===========");
+            System.out.println("        1.대기 등록 | 2.대기 현황");
             System.out.println("========================================");
-            System.out.print("선택>");
-            int choose=scan.nextInt();
+            System.out.print("선택 >");
+            int choose = scan.nextInt();
 
-            if(choose==1){
-                System.out.println("내용");
-                String content= scan.next();
-                System.out.println("작성자");
-                String writer= scan.next();
+            if (choose == 1) {
+                System.out.print("전화번호 :");
+                String phone = scan.next();
+                System.out.print("인원수:");
+                int count = scan.nextInt();
 
-                if(content1==null){
-                    content1=content;
-                    writer1= writer;
-                    System.out.println("[안내]글쓰기 성공");
-                }else if(content2 == null){
-                    content2=content;
-                    writer2= writer;
-                    System.out.println("[안내]글쓰기 성공");
-                }else if(content3== null){
-                    content3=content;
-                    writer3=writer;
-                    System.out.println("[안내]글쓰기 성공");
-                }else{
-                    System.out.println("[경고] 게시물을 등록할 공간이 부족합니다.");
+                if (phone1 == null) {
+                    phone1 = phone;
+                    count1 = count;
+                    System.out.println("[안내] 대기 등록이 완료되었습니다.");
+                } else if (phone2 == null) {
+                    phone2 = phone;
+                    count2 = count;
+                    System.out.println("[안내] 대기 등록이 완료되었습니다.");
+                } else if (phone3 == null) {
+                    phone3 = phone;
+                    count3 = count;
+                    System.out.println("[안내] 대기 등록이 완료되었습니다.");
+                } else {
+                    System.out.println("[경고] 더 이상 대기 등록을 할 수 없습니다.");
                 }
             }
-            if(choose==2){
-                System.out.println("============== 게시물 목록 ==============");
-                System.out.println("작성자 :");
-                System.out.println("내용:");
-                System.out.println("========================================");
-            }
-            if(content1!=null){
-                System.out.println("============== 게시물 목록 ==============");
-                System.out.println("작성자 :"+writer1);
-                System.out.println("내용:"+content2);
-                System.out.println("========================================");
-            }
-            if(content2!=null){
-                System.out.println("============== 게시물 목록 ==============");
-                System.out.println("작성자 :"+writer2);
-                System.out.println("내용:"+content2);
-                System.out.println("========================================");
-            }
-            if(content3!=null){
-                System.out.println("============== 게시물 목록 ==============");
-                System.out.println("작성자 :"+writer3);
-                System.out.println("내용:"+content3);
-                System.out.println("========================================");
+            if (choose == 2) {
+                System.out.println(" ============= 대기 현황 ============= :");
+                System.out.println(" ------------------------------------ :");
+
+                if (count1 != 0) {
+                    System.out.println(" ============= 대기 현황 ============= :");
+                    System.out.println(" 전화번호: " + phone1);
+                    System.out.println(" 인원수: " + count1);
+                    System.out.println(" ------------------------------------ :");
+
+                }
+                if (count2 != 0) {
+                    System.out.println(" ============= 대기 현황 ============= :");
+                    System.out.println(" 전화번호: " + phone2);
+                    System.out.println(" 인원수: " + count2);
+                    System.out.println(" ------------------------------------ :");
+
+                }
+                if (count3 != 0) {
+                    System.out.println(" ============= 대기 현황 ============= :");
+                    System.out.println(" 전화번호: " + phone3);
+                    System.out.println(" 인원수: " + count3);
+                    System.out.println(" ------------------------------------ :");
+
+                }
             }
         }
-
-    }//main e
-}//class e
+    }
+}
---
+++ src/종합/예제2/BoardService2.java
@@ -0,0 +1,65 @@
+package 종합.예제2;
+
+import java.util.Scanner;
+
+public class BoardService2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);                                    //(3)입력객체 생성
+
+        // (5) 메모리 설계
+        // boardService1 중에서는 총 게시물 3개를 저장,      변수     --> 100개 --> 200개 변수 필요
+        // boardService2 총 1게시물 100를 저장, 변수+배열            --> 100개 --> 배열2개 변수 필요
+        String[] contents = new String[100];      // 총 문자열(String) 100개를 저장할수 있는 배열 선언
+        String[] writers = new String[100];       // 총 문자열(String) 100개를 저장할수 있는 배열 선언
+
+        for (; ; ) {                                                            //(1) 무한루프
+            System.out.println("============= My Community =============");     //(2) 출력
+            System.out.println("1.게시물쓰기 | 2.게시물출력");
+            System.out.println("========================================");
+            System.out.print("선택 >");
+            int choose = scan.nextInt();
+
+
+            if (choose == 1) {                                                      //(4)'1`번 입력시
+                System.out.print("내용:");
+                String content = scan.next();
+                System.out.print("작성자:");
+                String writer = scan.next();
+                boolean check = false; //false: 저장실패, true:저장성공
+                for (int i = 0; i <= contents.length - 1; i++) {
+                    String cont = contents[i];
+                    if (cont == null) { // 만약에 index번째가  null(비어있으면)이면
+                        contents[i] = content; // 입력받은 값 대입한다.
+                        writers[i] = writer;
+                        check = true;
+                        break;      //저장 성공시 상태변수 수정 후 반복문 탈출
+                    }
+                }
+                if (check) {
+                    System.out.println("[안내]글쓰기 성공");
+                } else {
+                    System.out.println("[경고] 게시물을 등록할 공간이 부족합니다.");
+                }
+            } else if (choose == 2) {                                                //(4)'2'번 입력시
+                System.out.println("============= 게시물 목록 =============");
+                for (int j = 0; j <= contents.length - 1; j++) {
+                    String cont = contents[j];
+                    if (cont != null) {
+                        System.out.println("작성자:"+writers[j]);
+                        System.out.println("내용:"+contents[j]);
+                        System.out.println("------------------------------------");
+                    }
+                }
+
+
+            }
+        }
+        //step1: 반복적인 메인(화면출력)출력,입력
+        //step2:입력 선택에 따른 화면 출력
+        //step2: 입력 선택에 따른 화면 출력
+        //step3: 기능별 필요한 메모리 설계
+        //step4: 각 기능별 세부 코드 구현
+
+
+    } //main e
+}// class e
