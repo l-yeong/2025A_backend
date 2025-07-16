cffef74be563712e1135d91be37e930437a8937f
l-yeong <yahn10@naver.com>
2025-07-16 11:37:20 +0900

    DB커밋

---
+++ src/day13_20250716/Example1.java
@@ -0,0 +1,10 @@
+package day13_20250716;
+
+public class Example1 {
+    public static void main(String[] args) {
+        /*
+        [ 상속 ]
+            1. 정의: (상위)클래스 로부터 (하위)클래스 에게 (멤버) 물려주는 행위
+        */
+    }
+}
